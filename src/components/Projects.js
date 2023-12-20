052bba190e40913641cb461a3616fe18b0e00641
Jiong Qi <jiongqipan@gmail.com>
2023-12-19 23:58:36 -0500

    Setup Mailer Form

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -41,7 +41,7 @@ export const Projects = () => {
             <h2>Projects</h2>
             <p></p>
             <Tab.Container id="projects-tabs" defaultActiveKey="first">
-              <Nav variant="pills" defaultActiveKey="/home">
+              <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                 <Nav.Item>
                   <Nav.Link eventKey="first">Tab One</Nav.Link>
                 </Nav.Item>
